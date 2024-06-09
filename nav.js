c3d982b604441f5d51af6c77fa740945ca246ac1
Samour Chandra Paul <scpsamour@gmail.com>
2024-06-09 12:17:45 +0600

    i have change the navbar to responsive slidbar nav

---
+++ nav.js
@@ -0,0 +1,8 @@
+function slidebar(){
+    const humber = document.querySelector('.nav-responsive')
+    humber.style.display = "none"
+}
+function sidelogo(){
+    var humber= document.querySelector('.nav-responsive')
+    humber.style.display ='inline'
+}
